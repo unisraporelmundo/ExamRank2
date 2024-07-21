5482f35c088b33e59fab95d1d732db0a0e959555
Isra Izquierdo <theviking@MacBook-Pro-de-Isra.local>
2024-07-21 15:21:23 +0200

    ExamRank02

---
+++ ft_putstr.c
@@ -0,0 +1,15 @@
+/*Escribe una función que muestre una cadena en la salida estándar.*/
+
+#include "unistd.h" // Librería que contiene la función write //
+
+void    ft_putstr(char *str) // Función que recibe un puntero a un array de caracteres //
+{
+    int i; // Variable entera //
+
+    i = 0; // Inicializa la variable en 0 //
+    while (str[i]) // Mientras el string exista //
+    {
+        write (1, &str[i], 1); // Escribe en la salida estándar el caracter en la posición actual que ocupa un byte //
+        i++; // Incrementa la variable en 1 //
+    }
+}
